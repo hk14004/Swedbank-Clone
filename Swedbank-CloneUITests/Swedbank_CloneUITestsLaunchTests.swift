0f7685463335dc2c62bf628222a515c8d2fd7baf
hk14004 <hardijs.kirsis@gmail.com>
2023-05-17 18:36:36 +0300

    Initial Commit

---
+++ Swedbank-CloneUITests/Swedbank_CloneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Swedbank_CloneUITestsLaunchTests.swift
+//  Swedbank-CloneUITests
+//
+//  Created by Hardijs Ķirsis on 17/05/2023.
+//
+
+import XCTest
+
+final class Swedbank_CloneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
